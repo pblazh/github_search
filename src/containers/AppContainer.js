89573dc25e34c205f0539596ef6a1a1e98822289
Pavel Blazhyievskyi <p.blazhyievskyi@gmail.com>
2017-09-10 22:08:46 +0200

    built version

--- src/containers/AppContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../stylesheets/main.css'
-
-export default (props) => (
-	<div className='appContainer'>
-		<header className='App-header'>Github search</header>
-		{props.children}
-	</div>
-)
--- src/util/index.js
+++
@@ -1,8 +0,0 @@
-const where = (what, list) =>
-	list.filter(item =>
-		Object.keys(what).reduce((pass, key) =>
-			pass && item[key] === what[key], true));
-
-export {
-	where,
-};
